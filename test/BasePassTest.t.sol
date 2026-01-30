f2d062e578bfc8f82502ba684fd7856562246edf
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-30 13:17:03 +0100

    BasePass Deploy Script base

--- test/BasePassTest.t.sol
+++ test/BasePassTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 // import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test {
+contract BasePassTest is Test {
     // Counter public counter;
 
     function setUp() public {
