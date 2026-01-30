34323f77d95c0cba92d8f7c16c09affbf10d7c67
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-30 14:09:22 +0100

    BasePassERC721.sol

---
+++ src/BasePassERC721.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BasePassERC721 is ERC721, Ownable {
+    uint256 public totalSupply;
+    address public metadataRenderer;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address creator
+    ) ERC721(name, symbol) Ownable(msg.sender) {
+        transferOwnership(creator);
+    }
+
+    function mint(address to) external onlyOwner {
+        totalSupply++;
+        _mint(to, totalSupply);
+    }
+
+    function setMetadataRenderer(address renderer) external onlyOwner {
+        metadataRenderer = renderer;
+    }
+}
