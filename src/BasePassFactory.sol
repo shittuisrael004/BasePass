64909d8ad7cf6be2a254629b2f96cf915f168e05
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-30 15:50:38 +0100

    create BasePassFactory contract

---
+++ src/BasePassFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./BasePassERC721.sol";
+
+contract BasePassFactory {
+    address public owner;
+    address[] public allClubs;
+
+    event ClubCreated(address club, address creator);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createClub(
+        string memory name,
+        string memory symbol
+    ) external returns (address) {
+        BasePassERC721 club = new BasePassERC721(name, symbol, msg.sender);
+        allClubs.push(address(club));
+        emit ClubCreated(address(club), msg.sender);
+        return address(club);
+    }
+
+    function getAllClubs() external view returns (address[] memory) {
+        return allClubs;
+    }
+}
