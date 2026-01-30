88c9beed2cc321adc1e7bce76311503dfc707e33
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-31 00:21:49 +0100

    create RewardDistributor contract

---
+++ src/RewardDistributor.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./RewardToken.sol";
+
+contract RewardDistributor {
+    RewardToken public token;
+
+    constructor(address tokenAddress) {
+        token = RewardToken(tokenAddress);
+    }
+
+    function distribute(address to, uint256 amount) external {
+        token.mint(to, amount);
+    }
+}
