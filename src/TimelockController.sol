52d7cdffce4cde05ddeaacb9da7bd675355a7b08
shittuisrael004 <shittuisrael004@gmail.com>
2026-02-02 00:26:00 +0100

    fix: mixed case variable lint

--- src/TimelockController.sol
+++ src/TimelockController.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.20;
 
 contract TimelockController {
-    address public ADMIN;
+    address public admin;
 
     uint256 public delay = 2 days;
 
     constructor() {
-        ADMIN = msg.sender;
+        admin = msg.sender;
     }
 
     function changeDelay(uint256 _days) public {
-        require(msg.sender == ADMIN, "Not authorized");
+        require(msg.sender == admin, "Not Admin");
         delay = _days * 1 days;
     }
 }
